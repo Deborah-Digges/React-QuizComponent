48f4410a2a9097d60e3ac0eeda4e2af63e56fc70
Deborah Gertrude Digges <DeborahGertrude_Digges@intuit.com>
2018-08-25 18:21:14 +0530

    Add QuizQuestionButton component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import QuizQuestionButton from './QuizQuestionButton.js';
+
 
 class QuizQuestion extends Component {
     render() {
@@ -8,7 +10,7 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            {this.props.quiz_question.answer_options[0]}
+            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
           </ul>
         </section>
       </main>);
