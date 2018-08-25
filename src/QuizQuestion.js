e4b6684d8e2aa6878ddb3fedd88693f2d61bf2c5
Deborah Gertrude Digges <DeborahGertrude_Digges@intuit.com>
2018-08-25 19:03:11 +0530

    Implement error handling

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,23 @@ import QuizQuestionButton from './QuizQuestionButton.js';
 
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            incorrectAnswer: false
+        };
+    }
+
     handleClick(buttonText) {
         if(this.props.quiz_question.answer === buttonText) {
             this.props.showNextQuestionHandler();
+            this.setState({
+                incorrectAnswer: false
+            });
+        } else {
+            this.setState({
+                incorrectAnswer: true
+            });
         }
     }
 
@@ -20,6 +34,11 @@ class QuizQuestion extends Component {
                 (answer_option, index) => (<QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option}/>)
 
             )}
+
+            {this.state.incorrectAnswer?
+                <p className="error">Sorry, thats not right</p>
+                : null
+            }
 
           </ul>
         </section>
